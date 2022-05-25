72763556147ef3f278d925eb1820fcb4b481dc8a
ganden16 <alvindian85@gmail.com>
2022-05-25 20:28:42 +0700

    finishing dan responsivee

--- resources/views/dashboard/layouts/header.blade.php
+++ resources/views/dashboard/layouts/header.blade.php
@@ -1,5 +1,5 @@
 <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-    <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="/posts">Back to Home</a>
+    <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="/posts">Back to Posts</a>
     <input class="form-control form-control-dark w-100 bg-dark" type="text" readonly>
     <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
